69b1e039155485b3339bb79dfd7db46e71dffbfa
Nadya Yahuili Avendaño Chavez <nadya.25yahui@gmail.com>
2025-09-04 12:18:50 -0600

    Add files via upload

---
+++ Ejercicio2/src/Ejercicio2.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Ejercicio2 {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton guardarButton;
+    private JButton salirButton;
+}
---
+++ Ejercicios/src/Ejercicio_1.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Ejercicio_1 {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton guardarButton;
+    private JButton cancelarButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
