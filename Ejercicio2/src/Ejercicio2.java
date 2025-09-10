238d9269842033a33fbc02e94630db75f06eb5c5
Nadya Yahuili Avendaño Chavez <nadya.25yahui@gmail.com>
2025-09-10 15:24:24 -0600

    Delete Ejercicio2 directory

--- Ejercicio2/src/Ejercicio2.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Ejercicio2 {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JButton guardarButton;
-    private JButton salirButton;
-}
