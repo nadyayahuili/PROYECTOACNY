b56d8090dbde613488c3df4b02f3607a8935a377
Nadya Yahuili Avendaño Chavez <nadya.25yahui@gmail.com>
2025-09-10 15:19:40 -0600

    Add files via upload
    Ejercicio 1 Practica de login

---
+++ Ejercicio_1/src/Ejercicio_1_iniciar_sesion.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Ejercicio_1_iniciar_sesion {
+    private JPanel panel_principal;
+    private JPanel panel_defondo;
+    private JLabel WELCOMEHOMELabel;
+    private JTextField emailTextField;
+    private JPasswordField passwordPasswordField;
+    private JCheckBox rememberFor30DaysCheckBox;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
